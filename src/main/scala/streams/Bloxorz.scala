82003562ba55ef4a9ffb944bd69efdff3b464faa
Carl Kristensen <carl.kristensen@klavenessdigital.com>
2021-03-06 13:56:43 +0100

    solved

---
+++ src/main/scala/streams/Bloxorz.scala
@@ -0,0 +1,51 @@
+package streams
+
+/**
+ * A main object that can be used to execute the Bloxorz solver
+ */
+object Bloxorz extends App {
+
+  /**
+   * A level constructed using the `InfiniteTerrain` trait which defines
+   * the terrain to be valid at every position.
+   */
+  object InfiniteLevel extends Solver with InfiniteTerrain {
+    val startPos = Pos(1,3)
+    val goal = Pos(5,8)
+  }
+ // println(InfiniteLevel.solution)
+  println(InfiniteLevel.pathsFromStart.take(10))
+
+  /**
+   * A simple level constructed using the StringParserTerrain
+   */
+  abstract class Level extends Solver with StringParserTerrain
+
+  object Level0 extends Level {
+    val level =
+      """------
+        |--ST--
+        |--oo--
+        |--oo--
+        |------""".stripMargin
+  }
+
+  //println(Level0.solution)
+  println(Level0.pathsFromStart.take(10))
+
+  /**
+   * Level 1 of the official Bloxorz game
+   */
+  object Level1 extends Level {
+    val level =
+      """ooo-------
+        |oSoooo----
+        |ooooooooo-
+        |-ooooooooo
+        |-----ooToo
+        |------ooo-""".stripMargin
+  }
+
+  //println(Level1.solution)
+  println(Level1.pathsFromStart.take(10))
+}
---
+++ src/main/scala/testing.sc
@@ -0,0 +1,2 @@
+import streams.Solver
+import streams._
